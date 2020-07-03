1f1f2ca5cb85a66efb4bc249a98ced8d03f0dbaf
unknown <ibnutriyuono@gmail.com>
2020-07-03 21:21:04 +0700

    return register screen for development

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
-import { LoginScreen } from "./containers/pages";
+import { LoginScreen, RegisterScreen } from "./containers/pages";
 
 export default class index extends Component {
   render() {
-    return <LoginScreen />;
+    // return <LoginScreen />;
+    return <RegisterScreen />;
   }
 }
