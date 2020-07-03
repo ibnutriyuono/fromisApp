ba2b9e391bff4a57fa58e41a40516831d6262cae
unknown <ibnutriyuono@gmail.com>
2020-07-03 14:16:01 +0700

    adding color pallete

---
+++ src/styles/pallete.js
@@ -0,0 +1,10 @@
+export const color = {
+  lightBg: "#f2f0f5",
+  darkBg: "#b9add3",
+  lightHl: "#ccc3df",
+  darkHl: "#ccccdd",
+  purple: "#8c7ab6",
+  purpleDark: "#ac9bc9",
+  purpleLight: "#a494dc",
+  pupleDarker: "#6c5490",
+};
