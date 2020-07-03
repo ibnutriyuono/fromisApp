5ee35661c56b0169155fc96d0f35ee1590481d57
unknown <ibnutriyuono@gmail.com>
2020-07-03 20:39:54 +0700

    adding textinput components

---
+++ src/components/atoms/TextInputPassword.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { TextInput, StyleSheet, View } from "react-native";
+import { colors } from "../../styles";
+
+export default class Form extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.input}
+          placeholder={this.props.placeholder}
+          placeholderTextColor={colors.purpleLight}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 25,
+  },
+  input: {
+    height: 40,
+    backgroundColor: "white",
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 14,
+  },
+});
