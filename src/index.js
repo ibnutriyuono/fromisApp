bb873d4534e654152a2e46b0d10eadf4cf1f201d
unknown <ibnutriyuono@gmail.com>
2020-07-03 21:15:01 +0700

    get rid unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { colors } from "./styles";
 
-import LoginScreen from "./containers/pages/LoginScreen";
+import { LoginScreen } from "./containers/pages";
 
 export default class index extends Component {
   render() {
     return <LoginScreen />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.lightBg,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
